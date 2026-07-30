d091f17da9344e05b504ac77e4cb8abf57be1d8b
Harry <harrybryant16@gmail.com>
2026-07-30 14:07:07 +0100

    simplication of null init

--- ETCSDC-Properties/Operators/Predicate.cs
+++ ETCSDC-Properties/Operators/Predicate.cs
@@ -12,6 +12,6 @@
 
         /// Property to get and set the terms passed as arguements to the predicate.
         [System.Xml.Serialization.XmlElement("Term", typeof(SwanLLVerifier.ETCSDC_Properties.Term), Namespace = Constants.NAMESPACE)]
-        public SwanLLVerifier.ETCSDC_Properties.Term[] Term { get; set; } = Array.Empty<Term>();        
+        public SwanLLVerifier.ETCSDC_Properties.Term[] Term { get; set; } = [];        
     }
 }

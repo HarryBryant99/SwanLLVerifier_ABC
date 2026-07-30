3ff7789c280877ddc3bfec279839fa15293202fe
Harry <harrybryant16@gmail.com>
2026-07-30 13:41:06 +0100

    ladder parser null fix v2

--- TptpParser/LadderParser.cs
+++ TptpParser/LadderParser.cs
@@ -21,7 +21,7 @@ namespace SwanLLVerifier.TptpParser
             using (var reader = new StreamReader(inputStream, Encoding.UTF8))
             {
                 string line;
-                while ((line = reader.ReadLine()) != null)
+                while ((line = reader.ReadLine()!) != null)
                 {
                     if (string.IsNullOrWhiteSpace(line))
                     {
